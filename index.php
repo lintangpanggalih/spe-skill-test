280f4be44ca723bd9ce45558870e0ca3d2d813f7
Sasenna <lintangpanggalih@gmail.com>
2022-07-16 11:27:10 +0700

    add parity outlier func

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ function main()
 
     $spe->narcissisticNumber(135);
     $spe->narcissisticNumber(153);
+
+    $spe->parityOutlier([2, 4, 0, 100, 4, 11, 2602, 36]);
+    $spe->parityOutlier([11, 13, 15, 19, 9, 13, -21]);
 }
 main();
 
@@ -38,8 +41,20 @@ class SpeSkillTest
         var_dump($result == $number ? true : false);
     }
 
-    public static function parityOutlier()
+    public static function parityOutlier($array)
     {
-        
+        $even = 0;
+        $odd = 0;
+        $cycle = 0;
+
+        foreach ($array as $value) {
+            if ($value % 2 == 0) {
+                $cycle++;
+                $even = $value;
+            } else {
+                $odd = $value;
+            }
+        }
+        var_dump(($cycle > 1) ? $odd : $even);
     }
 }
